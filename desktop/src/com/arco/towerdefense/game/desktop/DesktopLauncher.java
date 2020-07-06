939b1b6a1fa26f575108683f08eec09a340dbd59
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-07-06 12:42:14 -0300

    Add a groundController and it will take care of all the map

--- desktop/src/com/arco/towerdefense/game/desktop/DesktopLauncher.java
+++ desktop/src/com/arco/towerdefense/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.arco.towerdefense.game.TowerDefenseGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Tower Defense";
+		config.width = 800;
+		config.height = 480;
+
 		new LwjglApplication(new TowerDefenseGame(), config);
 	}
 }
