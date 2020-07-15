14da604dba4afa9bc7f32ca009c13fdb353a8615
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-07-15 17:50:24 -0300

    InteractionZone to use the new StackLayout

--- core/src/com/arco/towerdefense/game/controllers/InteractionZoneController.java
+++ core/src/com/arco/towerdefense/game/controllers/InteractionZoneController.java
@@ -1,15 +1,66 @@
 package com.arco.towerdefense.game.controllers;
 
+import com.arco.towerdefense.game.GameSingleton;
 import com.arco.towerdefense.game.drawer.InteractionZoneDrawer;
+import com.arco.towerdefense.game.layouts.StackLayout;
+import com.arco.towerdefense.game.layouts.enums.Orientation;
+import com.arco.towerdefense.game.layouts.enums.Position;
+import com.arco.towerdefense.game.layouts.interfaces.LayoutListener;
+import com.arco.towerdefense.game.layouts.wrappers.LayoutWrapper;
+import com.arco.towerdefense.game.utils.Consts;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class InteractionZoneController {
     InteractionZoneDrawer interactionZoneDrawer;
+    StackLayout stackSelectionTowers;
+
     public InteractionZoneController(SpriteBatch batch) {
         interactionZoneDrawer = new InteractionZoneDrawer(batch);
+
+        stackSelectionTowers = new StackLayout(batch, Orientation.HORIZONTAL);
+        stackSelectionTowers.setMarginBetween(3);
+        stackSelectionTowers.setPadding(3);
+
+        initSelectionTowers();
     }
 
     public void update() {
-        interactionZoneDrawer.drawSelectionTowers();
+        stackSelectionTowers.drawAtPos(Position.BOTTOM_LEFT);
+        handleMouse();
+    }
+
+    private void initSelectionTowers() {
+        String[] selectionTowers = {
+                Consts.TOWER_GLOBULO_BRANCO_SELECTION,
+                Consts.TOWER_GLOBULO_BRANCO_SELECTION,
+                Consts.TOWER_GLOBULO_BRANCO_SELECTION,
+                Consts.TOWER_GLOBULO_BRANCO_SELECTION
+        };
+
+        for (String selectionTower: selectionTowers) {
+            stackSelectionTowers.addWrapper(
+                new LayoutWrapper(
+                    new Sprite(GameSingleton.getInstance().getTexture(selectionTower)),
+                    new LayoutListener() {
+                        @Override
+                        public void onClick(LayoutWrapper layoutWrapper) {
+                            System.out.printf("CLICADO EM\n");
+                        }
+
+                        @Override
+                        public void onHover(LayoutWrapper layoutWrapper) {
+                            System.out.printf("ON HOVER EM\n");
+                        }
+                    }
+                )
+            );
+        }
+    }
+
+    private void handleMouse() {
+        if (stackSelectionTowers.isCursorInside()) {
+            stackSelectionTowers.handleMouse();
+        }
     }
 }
--- core/src/com/arco/towerdefense/game/drawer/InteractionZoneDrawer.java
+++ core/src/com/arco/towerdefense/game/drawer/InteractionZoneDrawer.java
@@ -2,34 +2,16 @@ package com.arco.towerdefense.game.drawer;
 
 import com.arco.towerdefense.game.GameSingleton;
 import com.arco.towerdefense.game.layouts.StackLayout;
+import com.arco.towerdefense.game.layouts.interfaces.LayoutListener;
+import com.arco.towerdefense.game.layouts.wrappers.LayoutWrapper;
 import com.arco.towerdefense.game.utils.Consts;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class InteractionZoneDrawer {
-    StackLayout stackSelectionTowers;
     SpriteBatch batch;
 
     public InteractionZoneDrawer(SpriteBatch batch) {
         this.batch = batch;
-        stackSelectionTowers = new StackLayout(batch, StackLayout.ORIENTATION_HORIZONTAL);
-
-        initSelectionTowers();
-    }
-
-    private void initSelectionTowers() {
-        String[] selectionTowers = {
-                Consts.TOWER_GLOBULO_BRANCO_SELECTION
-        };
-
-        for (String selectionTower: selectionTowers) {
-            stackSelectionTowers.addSprite(
-                    new Sprite(GameSingleton.getInstance().getTexture(selectionTower))
-            );
-        }
-    }
-
-    public void drawSelectionTowers() {
-        stackSelectionTowers.drawAtPos(StackLayout.POSITION_BOTTOM_LEFT);
     }
 }
